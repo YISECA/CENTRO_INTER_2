5df25da70a7a34190d07b85a4b36429131141855
YISECA <yiseca@gmail.com>
2017-05-17 17:15:28 -0500

    inicio del formulario

---
+++ resources/views/email.blade.php
@@ -0,0 +1,13 @@
+<html>
+
+
+
+<h1>Caminata CENTRO INTERNACIONAL 19 de MARZO de 2017</h1>
+
+
+
+<h1>Su registro fue exitoso!</h1>
+
+
+
+<p></p>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,288 @@
+@extends('master')                              
+
+@section('content')         
+
+<link rel="stylesheet" type="text/css" href="public/Css/form.css">
+
+<link rel="stylesheet" type="text/css" href="public/Css/slider_styles.css">
+
+<section id="page1">
+
+    <div class="panel panel-default">
+
+        <div class="panel-heading">Inicio</div>
+
+        <div class="panel-body">
+
+            <div class="freebirdFormviewerViewFormContent ">
+
+                <div class="freebirdFormviewerViewHeaderHeader">
+
+                    <div class="freebirdFormviewerViewHeaderTitleRow">
+
+                        <div class="freebirdFormviewerViewHeaderTitle" dir="auto" role="heading" aria-level="1">Caminata CENTRO INTERNACIONAL 19 de MARZO de 2017</div>
+
+                    </div>
+<br>
+                    <div class="stage">
+
+                        <div id="SLDR-ONE" class="sldr">
+
+                            <ul class="wrp animate">
+
+                                <li class="elmnt-one">                                 
+
+                                        <div class="wrap"><img src="public/Img/centro.jpg"  height="400">
+
+                                        </div>                                   
+                                </li>
+
+                               <li class="elmnt-two">
+                                    
+                                        <div class="wrap"><img src="public/Img/centro2.jpg"  height="400">
+
+                                        </div>                                 
+                                </li>
+
+                                 <li class="elmnt-three">
+                                    
+                                        <div class="wrap"><img src="public/Img/centro3.jpg"  height="400">
+
+                                        </div>                                 
+                                </li>
+                                 
+
+                            </ul>
+
+                        </div>
+
+                        <div class="clear"></div>
+
+                        <ul class="selectors">
+
+                            <li class="focalPoint"><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>            
+                         
+                        </ul>
+
+                    </div>
+
+                    <script src="public/Js/jquery.sldr.js"></script>
+
+                    <div class="freebirdFormviewerViewHeaderDescription" dir="auto">
+
+                        <h2>Ubicación</h2>
+
+                        <p><font size="3">El centro Internacional está ubicado en el centro oriente de Bogotá,  localidad 3° de Santa Fe. Allí se cuenta con uno de los parques más representativos de la ciudad, el parque Nacional y en muchos escenarios de su entorno tienen lugar acontecimientos históricos, políticos, sociales y económicos que trazan un recorrido único con una perspectiva moderna y patrimonial de la ciudad. El recorrido acompañado por la reconocida a nivel mundial “Ciclo vía” finaliza en el mercado de Pulgas San Alejo, declarado Patrimonio Cultural y turístico de Bogotá.</font></p>
+
+                        <h2>Descripción del recorrido </h2>
+
+                        <p><font size="3">El recorrido da inicio en el parque Nacional, en la torre del reloj, donde se realizará un calentamiento, el guía de caminatas del IDRD dará las recomendaciones propias del recorrido necesarias para su ejecución y se dará inicio al recorrido.</font></p>
+
+                        <h2>Durante el recorrido se visitaran los siguientes atractivos:</h2>
+                        <ul class="ul_inicio">
+                            <font size="3">
+                            <li>Parque Nacional</li>
+                            <li>Barrio La Merced</li>
+                            <li>Exteriores de: <strong>Museo Nacional, Parque Central Bavaria, Plaza de Toros, Torres del Parque, Planetario Distrital,Torre Colpatria y Museo de arte Moderno de Bogotá MAMBO</strong></li>
+                            <li>Parque de la Independencia</li>
+                            <li>Parque Bicentenario</li>
+                            <li>Mercado de Pulgas San Alejo</li>
+                            </font>
+                        </ul>
+                        <br>
+
+                        <font size="4"><center><table width="1000">
+
+                            <col width="281">
+
+                                <col width="282">
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Punto de encuentro</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Torre del reloj del Parque Nacional</p>
+
+                                        </td>
+
+                                    </tr>
+                              
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Fecha</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Domingo 19 de Marzo de 2017</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Hora</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>8:00 a.m</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Cupo Máximo</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>40 Personsa</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Rango de Edad</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>“APTA PARA TODA LA FAMILIA”</p>
+
+                                        </td>
+
+                                    </tr>
+
+
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Altitud</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>2.600 m.s.n.m</td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Temperatura</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>14°</p>
+
+                                        </td>
+
+                                    </tr>
+                                    <!--<tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Nivel</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Medio</p>
+
+                                        </td>
+
+                                    </tr>
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Distancia</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>3.5 km</p>
+
+                                        </td>
+
+                                    </tr>-->
+                                     
+
+                        </table></center></font>
+
+                        <h2>Recomendaciones</h2>
+
+                        <ul class="ul_inicio">
+
+                            <font size="3">
+
+                                <li>Llevar ropa cómoda</li>
+                            
+                                <li>Llevar bloqueador solar</li>
+
+                                <li>Hidratación</li>
+
+                                <li>Calzado cómodo</li>
+
+                                <li>Llevar carné de la eps</li>
+
+                                <li>Refrigerio.</li>
+
+                                <li>Medicamentos personales </li>
+
+                                <li>No llevar mascotas </li>
+
+                                <li>Abstenerse de participar: mujeres en estado de embarazo, personas que tengan problemas de salud complejos</li>
+
+                                <li><ins>Abstenerse de asistir si no se encuentra inscrito</ins></li>
+                            </font>
+
+                            <br>
+
+                        </ul> <a class="btn btn-info" href="registro">Registrarse</a> </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+</section>
+
+@stop
